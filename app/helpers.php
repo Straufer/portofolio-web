be7c020c48da453896d673d2b8bfb55e65d79eae
straufer <paungcuy15@gmail.com>
2023-10-25 14:31:27 +0700

    update helpers clean code

--- app/helpers.php
+++ app/helpers.php
@@ -4,13 +4,13 @@ use App\Models\Project;
 use App\Models\Section;
 use App\Models\Settings;
 
-function get_setting_value($key): Settings{
+function get_setting_value($key){
     $data = Settings::where('key', $key)->first();
     return $data = isset($data->value) ? $data->value : 'key is empty';
 }
 
 
-function get_section_data($key): Section{
+function get_section_data($key){
     $data = Section::where('post_as', $key)->first();
     return $data = isset($data) ? $data : 'cannot access section because key is valid';
 }
@@ -19,6 +19,10 @@ function get_project(){
     $data = Project::all();
     return $data;
 }
+
+    // project 
+    // $data = project::all();
+    // return $data;
 
 
 
